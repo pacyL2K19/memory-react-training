508ea551a687d52de5f282215d90b157d1a8d936
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-05 13:31:37 +0200

    GuessCount component

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 import './Card.css'
 
+// eslint-disable-next-line no-unused-vars
 const HIDDEN_SYMBOL = '❓'
 
-const Card = () => <div className="card" />
+const Card = ({card, feedback}) => (
+    <div className={`card ${feedback}`}>
+        <span className='symbol'>
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
+    </div>
+)
 
 export default Card
