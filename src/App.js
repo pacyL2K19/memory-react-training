984e9481a1e571d6db52bff060c3561e2e02f190
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-05 13:33:34 +0200

    App component : implementing elementary features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
-import './App.css'
-
-class App extends Component {
+import React from 'react';
+import Card from './Card'
+import GuessCount from './GuessCount'
+class App extends React.Component {
   render() {
-    return <div className="memory" />
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" />
+        <Card card="🎉" feedback="justMatched" />
+        <Card card="💖" feedback="justMismatched" />
+        <Card card="🎩" feedback="visible" />
+        <Card card="🐶" feedback="hidden" />
+        <Card card="🐱" feedback="justMatched" />
+      </div>
+    )
   }
 }
 
-export default App
+export default App; 
